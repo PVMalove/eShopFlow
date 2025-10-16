900a4b8350685646660b0eeeddc2d5ad6c9efe09
Pavel Malove <107810341+PVMalove@users.noreply.github.com>
2025-10-16 16:11:50 +0300

    добавил проект common.kernel

---
+++ src/Shared/Common.Kernel/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Common.Kernel.Abstractions.Messaging;
+
+public interface ICommand : IRequest<Unit>, IBaseCommand;
+
+public interface ICommand<out TResponse> : IRequest<TResponse>, IBaseCommand;
+
+public interface IBaseCommand;
---
+++ src/Shared/Common.Kernel/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Common.Kernel.Abstractions.Messaging;
+
+public interface IQuery<out TResponse> : IRequest<TResponse>, IBaseQuery
+    where TResponse : notnull;
+
+public interface IBaseQuery;
